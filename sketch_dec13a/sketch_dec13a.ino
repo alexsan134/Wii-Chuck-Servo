5a98505a4f7ec072a6fbaa8ed9e47b7e2027544e
Alex Santos <as62971@gmail.com>
2017-12-15 23:45:10 +0000

    Add files via upload

--- sketch_dec13a/sketch_dec13a.ino
+++ sketch_dec13a/sketch_dec13a.ino
@@ -4,12 +4,15 @@
 
 Wiichuck wii;
 Servo servo;
+Servo servoY;
 const int ServoPin = 3;
+const int ServoPinY = 4;
 
 
 void setup() {
   Serial.begin(9600);
   servo.attach(ServoPin);
+  servoY.attach(ServoPinY);
   wii.init();
   wii.calibrate();
 }
@@ -22,6 +25,9 @@ void loop() {
     int accelX = wii.accelX();
     int accelZ = wii.accelZ();
     int jxServo = joyX - 42;
+    int jyServo = joyY - 42;
+    int axServo = (accelX/2)-157;
+    int ayServo = (accelY/2)-187;
 
     if(jxServo < 0){
         jxServo = 0;
@@ -29,7 +35,27 @@ void loop() {
     if(jxServo > 180){
         jxServo = 180;
       }
-    servo.write(jxServo);
-    delay(5);
+       if(jyServo < 0){
+        jyServo = 0;
+      }
+    if(jyServo > 180){
+        jyServo = 180;
+      }
+
+       if(axServo < 0){
+        axServo = 0;
+      }
+    if(axServo > 180){
+        axServo = 180;
+      }
+       if(ayServo < 0){
+        ayServo = 0;
+      }
+    if(ayServo > 180){
+        ayServo = 180;
+      }
+    servo.write(axServo);
+    servoY.write(ayServo);
+    delay(1);
   }
 }
