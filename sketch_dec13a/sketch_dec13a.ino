7cc3cfe60048e1368c7fdbcff0810cfebaaddb26
Alex Santos <as62971@gmail.com>
2018-01-05 22:24:51 +0000

    Add files via upload

--- sketch_dec13a/sketch_dec13a.ino
+++ sketch_dec13a/sketch_dec13a.ino
@@ -46,22 +46,10 @@ void setup() {
   delay(200);
   noTone(bass);
   delay(3000);
-  digitalWrite(changeUp, HIGH);
-    digitalWrite(ledBack, LOW);
+  digitalWrite(ledBack, LOW);
   digitalWrite(ledFront, LOW);
 }
 
-void lightsBlink(int l1, int l2, int l3) {
-  tone(bass, 100);
-  digitalWrite(l1, LOW);
-  digitalWrite(l2, HIGH);
-  digitalWrite(l3, HIGH);
-  delay(100);
-  noTone(bass);
-  digitalWrite(l2, LOW);
-  digitalWrite(l3, LOW);
-  delay(100);
-}
 
 void lightsLeft() {
   tone(bass, 300);
@@ -99,142 +87,130 @@ void lightsTurn(int es) {
   digitalWrite(es, LOW);
 }
 
+void acelerate() {
+  lightsTurn(ledBack);
+  digitalWrite(motorL, HIGH);
+  digitalWrite(motorR, LOW);
+}
+void reverse() {
+  lightsOff(ledBack);
+  lightsTurn(ledFront);
+  digitalWrite(motorL, LOW);
+  digitalWrite(motorR, HIGH);
+}
+void stopIt() {
+  lightsTurn(ledBack);
+  lightsTurn(ledFront);
+  digitalWrite(motorL, LOW);
+  digitalWrite(motorR, LOW);
+}
+
 
 void loop() {
-    if(Serial.available() > 0){
-       state = Serial.read();
+  if (Serial.available() > 0) {
+    state = Serial.read();
   }
 
-  
-  if(digitalRead(changeUp) == HIGH){
+  if (digitalRead(changeUp) == HIGH) {
 
-  if (wii.poll()) {
-    int joyX = wii.joyX();
-    int joyY = wii.joyY();
-    int accelY = wii.accelY();
-    int accelX = wii.accelX();
-    int accelZ = wii.accelZ();
-    int jxServo = joyX - 42;
-    int jyServo = joyY - 42;
-    int axServo = (accelX / 2) - 157;
-    int ayServo = (accelY / 2) - 187;
-    if (jxServo < 0) {
-      jxServo = 0;
-    }
-    if (jxServo > 180) {
-      jxServo = 180;
-    }
-    if (jyServo < 0) {
-      jyServo = 0;
-    }
-    if (jyServo > 180) {
-      jyServo = 180;
-    }
+    if (wii.poll()) {
+      int jxServo = wii.joyX() - 42;
+      int jyServo = wii.joyY() - 42;
+      int axServo = (wii.accelY() / 2) - 157;
+      int ayServo = (wii.accelX() / 2) - 187;
 
-    if (axServo < 60) {
-      axServo = 60;
-    }
-    if (axServo > 110) {
-      axServo = 110;
-    }
-    if (ayServo < 0) {
-      ayServo = 0;
-    }
-    if (ayServo > 180) {
-      ayServo = 180;
-    }
-
-    if (wii.buttonZ() > 0) {
-      lightsOff(ledBack);
-    lightsTurn(ledFront);
-    digitalWrite(motorL, LOW);
-   digitalWrite(motorR, HIGH);
-    }
-   else {
-      lightsTurn(ledBack);
-      digitalWrite(motorL, LOW);
-   digitalWrite(motorR, LOW);
-    }
-    if(jyServo > 120){
-    digitalWrite(motorL, HIGH);
-   digitalWrite(motorR, LOW);
-    }else{
-     digitalWrite(motorL, LOW);
-   digitalWrite(motorR, LOW);
-    }
-
-    if (wii.buttonC() > 0) {
-       lightsOff(ledFront);
-    lightsTurn(ledBack);
-   if(jxServo < 60){
-        lightsLeft();
-    }
-    else if (jxServo > 120){
-        lightsRight();
+      if (jxServo < 0) {
+        jxServo = 0;
       }
-      else if(jxServo > 60 &&  jxServo < 120){
+      if (jxServo > 180) {
+        jxServo = 180;
+      }
+      if (jyServo < 0) {
+        jyServo = 0;
+      }
+      if (jyServo > 180) {
+        jyServo = 180;
+      }
+
+      if (axServo < 60) {
+        axServo = 60;
+      }
+      if (axServo > 110) {
+        axServo = 110;
+      }
+      if (ayServo < 0) {
+        ayServo = 0;
+      }
+      if (ayServo > 180) {
+        ayServo = 180;
+      }
+
+      if (jyServo > 120) {
+        if (wii.buttonZ() > 0) {
+          reverse();
+        } else {
+          acelerate();
+        }
+      } else {
+        if (wii.buttonZ() > 0) {
+          reverse();
+        }
+        else {
+          stopIt();
+        }
+      }
+
+      if (wii.buttonC() > 0) {
+        lightsOff(ledFront);
+        lightsTurn(ledBack);
+        if (jxServo < 60) {
+          lightsLeft();
+        }
+        else if (jxServo > 120) {
+          lightsRight();
+        }
+        else if (jxServo > 60 &&  jxServo < 120) {
           lightsOff(ledFront);
         }
-    } 
-    servo.write(axServo);
-  }
-  }else{
-
-
- if (state == 'a') {
-   lightsTurn(ledFront);
-  lightsTurn(ledBack);
-   digitalWrite(motorL, HIGH);
-   digitalWrite(motorR, LOW);
- }
- else if (state == 'b') {
-    lightsOff(ledFront);
- }
-
- 
- else if (state == 'c') { 
-   lightsOff(ledBack);
-    lightsTurn(ledFront);
-    digitalWrite(motorL, LOW);
-   digitalWrite(motorR, HIGH);
-   servo.write(ia*13);
- }
- else if (state == 'd') {
-    lightsTurn(ledBack);
-    lightsTurn(ledFront);
-    digitalWrite(motorL, LOW);
-   digitalWrite(motorR, LOW);
- }
-
-
-
- else if (state == 'e') {
-    lightsLeft();
- }
- else if (state == 'f') {
-lightsRight();
- }else{
-     h = state;
+      }
+      servo.write(axServo);
+    }
+  } else {
+    if (state == 'b') {
+      lightsOff(ledFront);
+    }
+    else if (state == 'c') {
+      reverse();
+      servo.write(ia * 13);
+    }
+    else if (state == 'd') {
+      stopIt();
+    }
+    else if (state == 'e') {
+      lightsLeft();
+    }
+    else if (state == 'f') {
+      lightsRight();
+    } else {
+      h = state;
       ia = h - '0';
-      
-    if(h > 110){
+
+      if (h > 110) {
         h = 110;
-     }
-     if(h < 60){
+      }
+      if (h < 60) {
         h = 60;
       }
-      if(digitalRead(ledBack) == HIGH){
-        digitalWrite(motorL, LOW);
-   digitalWrite(motorR, HIGH);
-      }else{
-      digitalWrite(motorL, HIGH);
-   digitalWrite(motorR, LOW);
+      if (digitalRead(ledBack) == HIGH) {
+        reverse();
+      } else {
+        acelerate();
       }
-      servo.write(ia*13);
-  }
-    
+      servo.write(ia * 13);
+    }
 
-delay(10);
+
+    delay(10);
   }
 
 }
